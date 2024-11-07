b84169cdc0d3bf9487511be7b934bb5c385b6c45
Satoru Homma <s23C1128DB@s.chibakoudai.jp>
2024-11-07 11:35:15 +0900

    test

--- test.bash
+++ test.bash
@@ -1,5 +1,6 @@
-
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Satoru Homma 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo ${1}行目が違うよ
@@ -7,9 +8,17 @@ ng () {
 }
 
 res=0
-a=山田	
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]	 || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
 
+out=$(echo | ./plus)
+[ "$?" = 1 ]     || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
 
