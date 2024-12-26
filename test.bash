df991a43ffbbbb1c7b1878bb3af772840eb71dd0
Satoru Homma <s23C1128DB@s.chibakoudai.jp>
2024-12-26 20:25:05 +0900

    remake

--- test.bash
+++ test.bash
@@ -14,51 +14,51 @@ res=0
 out=$(echo "3 5 2 1 0" | ./hitting_score)
 expected="打席数が安打・犠打・犠飛数と四死球数より少ないです。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 out=$(echo "0 5 5 3 0" | ./hitting_score)
 expected="打席数がゼロです。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 out=$(echo あ | ./hitting_score)
 expected="入力が不正です。空白区切りの自然数を5つ入力してください。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 # 空の入力
 out=$(echo | ./hitting_score)
 expected="入力が不正です。空白区切りの自然数を5つ入力してください。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 ### pitching_score コマンドのテスト ###
 # 異常な入力
 out=$(echo "1 あ 1" | ./pitching_score)
 expected="入力が不正です。空白区切りの自然数を3つ入力してください。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 out=$(echo "1 2 3" | ./pitching_score)
 expected="投球回数の端数は0, 1, 2のいずれかにしてください。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 out=$(echo "0 2 0" | ./pitching_score)
 expected="投球回数がゼロです。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 out=$(echo あ | ./pitching_score)
 expected="入力が不正です。空白区切りの自然数を3つ入力してください。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 # 空の入力
 out=$(echo | ./pitching_score)
 expected="入力が不正です。空白区切りの自然数を3つ入力してください。"
 [ "${out}" = "${expected}" ] || ng "$LINENO"
-[ "$?" -eq 1 ] || ng "$LINENO"
+[ $? -eq 1 ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo "全てのテストが成功しました！"
 exit $res
